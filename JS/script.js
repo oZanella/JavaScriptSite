352f411402e751e1e817aaf5d0b90be2a9e9def2
Zanella <henriquezanella19@gmail.com>
2025-01-28 15:28:02 -0300

    adicionado mensagem de erro ao tentar inserir um campo vazio

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,23 @@
 //função para criar nova tarefa
 function addTask() {
-    //mensagem adicionada 
-    let mensagem = "Tarefa adicionada com sucesso!";
-    //adicionado texto no campo com id=inputTask
+
     let textTask = document.getElementById("inputTask")
-    //defiido o texto em task usando o get por id=mensagem
     let task = textTask.value
-    document.getElementById("mensagem").textContent = mensagem;
-    //limpa o campo depois de informado o valor
-    textTask.value = ""
-    //apresenta o valor do id na listask
+
+    if (task == "") {
+        let mensageError = "Digite uma tarefa para adiciona-lá na lista"
+        document.getElementById("mensage").textContent = mensageError;
+    }
+
     let listTask = document.getElementById("listTask")
-    //cria um novo elemnto com element=li
     let newTask = document.createElement("li")
-    //nova tarefa contendo o texto que foi definido em task
     newTask.textContent = task
-    //Adicionando o novo item à lista
     listTask.appendChild(newTask)
+
+    let mensageSucess = "Tarefa adicionada com sucesso!";
+    document.getElementById("mensage").textContent = mensageSucess;
+
+    textTask.value = ""
+
+
 }
