c7a1ff8f08b516a809450977037cb19568a22bc1
Zanella <henriquezanella19@gmail.com>
2025-01-28 15:03:36 -0300

    adicionado comentarios

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,19 @@
 function addTask() {
     //mensagem adicionada 
     let mensagem = "Tarefa adicionada com sucesso!";
-    
+    //adicionado texto no campo com id=inputTask
     let textTask = document.getElementById("inputTask")
+    //defiido o texto em task usando o get por id=mensagem
     let task = textTask.value
     document.getElementById("mensagem").textContent = mensagem;
-
+    //limpa o campo depois de informado o valor
     textTask.value = ""
-
+    //apresenta o valor do id na listask
     let listTask = document.getElementById("listTask")
+    //cria um novo elemnto com element=li
     let newTask = document.createElement("li")
-
+    //nova tarefa contendo o texto que foi definido em task
     newTask.textContent = task
-
+    //Adicionando o novo item à lista
     listTask.appendChild(newTask)
-
 }
