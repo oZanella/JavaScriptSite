25541b4e35b3580636f64d7e3b0138756c433daa
Zanella <henriquezanella19@gmail.com>
2025-01-30 08:56:08 -0300

    adicionado comentários e finalizado logica de if/else

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,30 @@
 //função para criar nova tarefa
 function addTask() {
+    
+    //let por conta de reatribuir valores
+    const textTask = document.getElementById("inputTask")
+    //pega o valor de texto inserido no input e defini em task, trim() não permite inserir espaços no inicio e fim do text
+    let task = textTask.value.trim()
+    //const definida por conta de não alterar valor
+    const mensageAlert = document.getElementById("mensage")
 
-    let textTask = document.getElementById("inputTask")
-    let task = textTask.value
-
+    //if = SE e else = SE NAO 
     if (task == "") {
-        let mensageError = "Digite uma tarefa para adiciona-lá na lista"
-        document.getElementById("mensage").textContent = mensageError;
+        //mensagem de erro
+        let mensageError = "Digite uma tarefa para adiciona-lá na lista!"
+        mensageAlert.textContent = mensageError;
+    } else {
+        //mensagem de sucesso
+        let mensageSucess = "Tarefa adicionada com sucesso!";
+        mensageAlert.textContent = mensageSucess;
+        //cria um item (li) e inserir na (lista ul)
+        const listTask = document.getElementById("listTask")
+        let newTask = document.createElement("li")
+        newTask.textContent = task
+        listTask.appendChild(newTask)
     }
 
-    let listTask = document.getElementById("listTask")
-    let newTask = document.createElement("li")
-    newTask.textContent = task
-    listTask.appendChild(newTask)
-
-    let mensageSucess = "Tarefa adicionada com sucesso!";
-    document.getElementById("mensage").textContent = mensageSucess;
-
+    //limpa o input do user 
     textTask.value = ""
-
 
 }
