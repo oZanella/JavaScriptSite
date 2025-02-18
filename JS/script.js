e6f2848aa6f8ba34f963fa12b3292a792a235c73
Zanella <henriquezanella19@gmail.com>
2025-02-18 15:34:12 -0300

    alterado nome de variável

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,24 @@
 //função para criar nova tarefa
 function addTask() {
     
+    let tarefas = []
+
     //let por conta de reatribuir valores
     const textTask = document.getElementById("inputTask")
     //pega o valor de texto inserido no input e defini em task, trim() não permite inserir espaços no inicio e fim do text
     let task = textTask.value.trim()
     //const definida por conta de não alterar valor
-    const mensageAlert = document.getElementById("mensage")
+    const messageAlert = document.getElementById("message")
 
     //if = SE e else = SE NAO 
     if (task == "") {
         //mensagem de erro
-        let mensageError = "Digite uma tarefa para adiciona-lá na lista!"
-        mensageAlert.textContent = mensageError;
+        let messageError = "Digite uma tarefa para adiciona-lá na lista!"
+        messageAlert.textContent = messageError;
     } else {
         //mensagem de sucesso
-        let mensageSucess = "Tarefa adicionada com sucesso!";
-        mensageAlert.textContent = mensageSucess;
+        let messageSucess = "Tarefa adicionada com sucesso!";
+        messageAlert.textContent = messageSucess;
         //cria um item (li) e inserir na (lista ul)
         const listTask = document.getElementById("listTask")
         let newTask = document.createElement("li")
